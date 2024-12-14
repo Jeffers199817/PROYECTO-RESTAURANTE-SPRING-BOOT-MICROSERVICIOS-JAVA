3fe717c6537b54398db74d91ae74d42e83cf4de9
Jeffers199817 <jefferson.alquinga1998@gmail.com>
2024-12-13 23:26:02 -0500

    6. Integracion de las configuraciónes de microservicio de menu

--- restaurantemenu/src/main/java/com/milenyumsoft/restaurantemenu/service/IMenuService.java
+++ restaurantemenu/src/main/java/com/milenyumsoft/restaurantemenu/service/IMenuService.java
@@ -1,6 +1,17 @@
 package com.milenyumsoft.restaurantemenu.service;
 
+import java.awt.*;
+import java.util.List;
+
 public interface IMenuService {
 
+public void addMenu(Menu menu);
 
+public void deleteMenu(Long id);
+
+public List<Menu> getMenus();
+
+public Menu getMenu(Long id);
+
+public Menu updateMenu(Menu menu);
         }
--- restaurantemenu/src/main/java/com/milenyumsoft/restaurantemenu/service/MenuService.java
+++ restaurantemenu/src/main/java/com/milenyumsoft/restaurantemenu/service/MenuService.java
@@ -1,4 +1,31 @@
 package com.milenyumsoft.restaurantemenu.service;
 
-public class MenuService {
+import java.awt.*;
+import java.util.List;
+
+public class MenuService implements IMenuService{
+    @Override
+    public void addMenu(Menu menu) {
+
+    }
+
+    @Override
+    public void deleteMenu(Long id) {
+
+    }
+
+    @Override
+    public List<Menu> getMenus() {
+        return List.of();
+    }
+
+    @Override
+    public Menu getMenu(Long id) {
+        return null;
+    }
+
+    @Override
+    public Menu updateMenu(Menu menu) {
+        return null;
+    }
 }
