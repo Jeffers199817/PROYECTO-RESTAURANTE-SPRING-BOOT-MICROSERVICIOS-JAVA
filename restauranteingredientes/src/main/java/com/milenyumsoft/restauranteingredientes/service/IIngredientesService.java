ef5e99b1df60e02587857cf63661e8b972dd4459
Jeffers199817 <jefferson.alquinga1998@gmail.com>
2024-12-13 23:22:01 -0500

    5. Integración de los metodos crud para ingredientes

--- restauranteingredientes/src/main/java/com/milenyumsoft/restauranteingredientes/service/IIngredientesService.java
+++ restauranteingredientes/src/main/java/com/milenyumsoft/restauranteingredientes/service/IIngredientesService.java
@@ -1,4 +1,19 @@
 package com.milenyumsoft.restauranteingredientes.service;
 
+import com.milenyumsoft.restauranteingredientes.model.Ingrediente;
+
+import java.util.List;
+
 public interface IIngredientesService {
+
+    public void addIngrediente(Ingrediente ingrediente);
+
+    public void deleteIngrediente(Long id);
+
+    public List<Ingrediente> getIngredientes();
+
+    public Ingrediente getIngrediente(Long id);
+
+    public Ingrediente updateIngrediente(Ingrediente ingrediente);
+
 }
--- restauranteingredientes/src/main/java/com/milenyumsoft/restauranteingredientes/service/IngredientesService.java
+++ restauranteingredientes/src/main/java/com/milenyumsoft/restauranteingredientes/service/IngredientesService.java
@@ -1,4 +1,32 @@
 package com.milenyumsoft.restauranteingredientes.service;
 
-public class IngredientesService {
+import com.milenyumsoft.restauranteingredientes.model.Ingrediente;
+
+import java.util.List;
+
+public class IngredientesService implements IIngredientesService{
+    @Override
+    public void addIngrediente(Ingrediente ingrediente) {
+
+    }
+
+    @Override
+    public void deleteIngrediente(Long id) {
+
+    }
+
+    @Override
+    public List<Ingrediente> getIngredientes() {
+        return List.of();
+    }
+
+    @Override
+    public Ingrediente getIngrediente(Long id) {
+        return null;
+    }
+
+    @Override
+    public Ingrediente updateIngrediente(Ingrediente ingrediente) {
+        return null;
+    }
 }
