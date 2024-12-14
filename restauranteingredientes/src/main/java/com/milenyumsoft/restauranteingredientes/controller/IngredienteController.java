bc36d00ba9eb9872e983d83728b6dce608ba1605
Jeffers199817 <jefferson.alquinga1998@gmail.com>
2024-12-13 23:14:13 -0500

    4. Integración de todas las configutaciones para los microservicicos

---
+++ restauranteingredientes/src/main/java/com/milenyumsoft/restauranteingredientes/controller/IngredienteController.java
@@ -0,0 +1,7 @@
+package com.milenyumsoft.restauranteingredientes.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IngredienteController {
+}
--- restauranteingredientes/src/main/java/com/milenyumsoft/restauranteingredientes/model/Ingrediente.java
+++ restauranteingredientes/src/main/java/com/milenyumsoft/restauranteingredientes/model/Ingrediente.java
@@ -1,10 +1,14 @@
 package com.milenyumsoft.restauranteingredientes.model;
 
+import jakarta.persistence.ElementCollection;
+
 import java.util.List;
 
 public class Ingrediente {
 
     private Long id_ingrediente;
     private String nombre_ingrediente;
+
+    @ElementCollection
     private List<String> lista_platos;
 }
