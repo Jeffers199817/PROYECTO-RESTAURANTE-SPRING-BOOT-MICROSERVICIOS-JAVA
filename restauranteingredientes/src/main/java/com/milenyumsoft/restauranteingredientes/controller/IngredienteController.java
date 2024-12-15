bdadb20297d94e6df510091e7a325bb182c04809
Jeffers199817 <jefferson.alquinga1998@gmail.com>
2024-12-14 23:29:56 -0500

    Integración de los microservicios menu y ingredientes

--- restauranteingredientes/src/main/java/com/milenyumsoft/restauranteingredientes/controller/IngredienteController.java
+++ restauranteingredientes/src/main/java/com/milenyumsoft/restauranteingredientes/controller/IngredienteController.java
@@ -1,7 +1,24 @@
 package com.milenyumsoft.restauranteingredientes.controller;
 
+import com.milenyumsoft.restauranteingredientes.model.Ingrediente;
+import com.milenyumsoft.restauranteingredientes.service.IIngredientesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/ingrediente")
 public class IngredienteController {
+
+    @Autowired
+    private IIngredientesService ingredientesService;
+
+    @PostMapping("/crear")
+    public String crearIngrediente(@RequestBody Ingrediente ingrediente){
+        ingredientesService.addIngrediente(ingrediente);
+        return "Ingrediente creado, exitosamente";
+    }
+
 }
