0845414f19989e9c59c688b95731bd1ace781b48
Jeffers199817 <jefferson.alquinga1998@gmail.com>
2024-12-14 22:41:27 -0500

    Integración de service para el microservicio ingredientes

--- restauranteingredientes/src/main/java/com/milenyumsoft/restauranteingredientes/repository/IIngredienteRepository.java
+++ restauranteingredientes/src/main/java/com/milenyumsoft/restauranteingredientes/repository/IIngredienteRepository.java
@@ -2,6 +2,8 @@ package com.milenyumsoft.restauranteingredientes.repository;
 
 import com.milenyumsoft.restauranteingredientes.model.Ingrediente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface IIngredienteRepository extends JpaRepository<Ingrediente, Long> {
 }
--- restauranteingredientes/src/main/java/com/milenyumsoft/restauranteingredientes/service/IngredientesService.java
+++ restauranteingredientes/src/main/java/com/milenyumsoft/restauranteingredientes/service/IngredientesService.java
@@ -1,12 +1,21 @@
 package com.milenyumsoft.restauranteingredientes.service;
 
 import com.milenyumsoft.restauranteingredientes.model.Ingrediente;
+import com.milenyumsoft.restauranteingredientes.repository.IIngredienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class IngredientesService implements IIngredientesService{
+
+
+   @Autowired
+    private IIngredienteRepository ingredienteRepository;
     @Override
     public void addIngrediente(Ingrediente ingrediente) {
+
 
     }
 
